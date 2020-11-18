f2c613e243169e135d573839f4ff824df55ea6bf
Rimi <Rimi@Rimi-PC>
2020-11-18 22:47:01 +0530

    Added Comment

--- Test/src/com/subhra/test/TestJDBC.java
+++ Test/src/com/subhra/test/TestJDBC.java
@@ -9,12 +9,12 @@ public class TestJDBC {
 
 	public static void main(String[] args) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bithal","root","root");
+			Class.forName("com.mysql.cj.jdbc.Driver");	//Load Driver Class
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bithal","root","root");	//Create Connection
 			
-			PreparedStatement pst = con.prepareStatement("Select * from student");
-			ResultSet rs = pst.executeQuery();
-			while(rs.next())
+			PreparedStatement pst = con.prepareStatement("Select * from student");	//Prepare Statement
+			ResultSet rs = pst.executeQuery();	//Execute Query and store result in ResultSet
+			while(rs.next())	//Iterate through ResultSet
 			{
 				System.out.println("Name : "+rs.getString("name")+" & Roll No is : "+rs.getInt("roll"));
 			}
